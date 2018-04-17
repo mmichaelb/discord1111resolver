b1ea0fa74ac54c31ee3000cdcd0d8f4fccfdb4ee
mmichaelb <mmichaelb@protonmail.com>
2018-04-17 23:25:14 +0200

    Add debug messages to pkg package

--- pkg/handler.go
+++ pkg/handler.go
@@ -102,7 +102,7 @@ func (resolveHandler *ResolveHandler) Handle(session *discordgo.Session, message
 	// initiate params (everything after "<@DISCORD-ID> "
 	params = commandSplit[1:]
 	// handle bot mention
-	ok = resolveHandler.handleMention(messageEmbed, params)
+	ok = resolveHandler.handleMention(messageCreate, messageEmbed, params)
 	// check result
 	if ok {
 		messageEmbed.Color = embedSuccessColor
@@ -125,7 +125,7 @@ syntaxCheck:
 
 // handleMention is an internal function which is called if the message starts with "<@DISCORD-ID> ". It returns whether
 // the execution was a success and if not, which fields should be printed withing the error message.
-func (resolveHandler *ResolveHandler) handleMention(messageEmbed *discordgo.MessageEmbed, params []string) (ok bool) {
+func (resolveHandler *ResolveHandler) handleMention(messageCreate *discordgo.MessageCreate, messageEmbed *discordgo.MessageEmbed, params []string) (ok bool) {
 	// check params length
 	if len(params) != 2 {
 		return false
@@ -157,7 +157,19 @@ func (resolveHandler *ResolveHandler) handleMention(messageEmbed *discordgo.Mess
 		}}
 		return false
 	}
-	return resolveHandler.executeDNSRequest(messageEmbed, messageType, messageTypeString, domainName)
+	var shortenedDomainName string
+	if logrus.GetLevel() > logrus.DebugLevel {
+		if len(domainName) > 16 {
+			shortenedDomainName = domainName[:16]
+		}
+		logrus.WithField("domain-name", shortenedDomainName).WithField("channel-id", messageCreate.ChannelID).
+			WithField("message-id", messageCreate.ID).Debug("requesting DNS entry...")
+	}
+	ok = resolveHandler.executeDNSRequest(messageEmbed, messageType, messageTypeString, domainName)
+	if logrus.GetLevel() > logrus.DebugLevel {
+		logrus.WithField("id", messageCreate.ID).WithField("ok", ok).Debug("result of DNS request.")
+	}
+	return
 }
 
 func validateDNSMessageType(messageTypeString string) (messageType uint16, ok bool) {
